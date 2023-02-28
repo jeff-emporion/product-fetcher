8ff546fe9a3dd2e4ba4beb6c6dae35e115e9e51b
jevgenij.liachovic <jevgenij@rrr.lt>
2023-02-28 17:54:30 +0200

    Add endpoint logic to retrieve product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/get_product_data', name: 'productData', methods: ['POST'])]
-    public function productData(Request $request): Response
+    public function productData(Request $request, HttpClientInterface $client): Response
     {
         $data = json_decode($request->getContent(), true);
-        if (!isset($data['marketplace']) || !in_array($data['marketplace'],['ebay', 'amazon'])) {
+        if (!isset($data['marketplace']) || !in_array($data['marketplace'],['provider_a', 'provider_b'])) {
             return $this->json(['error' => 'Correct marketplace id should be set'], Response::HTTP_BAD_REQUEST);
         }
 
@@ -21,19 +22,36 @@ class ProductController extends AbstractController
             return $this->json(['error' => 'Item ID should be set and be numeric'], Response::HTTP_BAD_REQUEST);
         }
 
-
-
-        return $this->json(
-            [
-                'data' => [
-                    'id' => $data['item_id'],
-                    'name' => 'name',
+        $productData = [];
+        switch ($data['marketplace']) {
+            case 'provider_a':
+                $response = $client->request('GET', sprintf('http://api1mock:8080/products/%s', $data['item_id']));
+                $body = json_decode($response->getContent(), 'json');
+                $productData = [
+                    'id' => $body['id'],
+                    'name' => $body['productName'],
                     'price' => [
-                        'amount' => 10,
-                        'currency' => 'EUR',
+                        'amount' => $body['productPrice'],
+                        'currency' => 'USD',
                     ],
-                ],
-            ]
-        );
+                ];
+
+                break;
+            case 'provider_b':
+                $response = $client->request('GET', sprintf('http://api2mock:8080/get_product?id=%s', $data['item_id']));
+                $body = json_decode($response->getContent(), 'json');
+                $productData = [
+                    'id' => $body['id'],
+                    'name' => $body['name'],
+                    'price' => [
+                        'amount' => $body['price'] * 1.2, //convert to USD
+                        'currency' => 'USD',
+                    ],
+                ];
+
+                break;
+        }
+
+        return $this->json($productData);
     }
 }
